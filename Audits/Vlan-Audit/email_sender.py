5776b42f3a254f1bd58363256f8a7e16e75d6b68
PyVold <92513901+PyVold@users.noreply.github.com>
2021-10-14 11:40:10 +0200

    Update email_sender.py

--- Audits/Vlan-Audit/email_sender.py
+++ Audits/Vlan-Audit/email_sender.py
@@ -6,15 +6,14 @@ def send_now(missing):
 
     from email.message import EmailMessage
 
-    sender = 'Nexus Vlan Audit <no_reply@bics.com>'
-    receivers = ['osama.aboelfath@bics.com']
+    sender = 'Nexus Vlan Audit <no_reply@ipdevops.com>'
+    receivers = ['osama@ipdevops.com']
     msg = EmailMessage()
     
 
 
     if missing is True:
-        msg_body = """ Nexus audit report is ready and missing vlans were discovered
-Find that attached report!"""
+        msg_body = """ Nexus audit report is ready and missing vlans were discovered, Find that attached report!"""
         msg.set_content(msg_body)
         #msg.add_header('Content-Disposition', 'attachment', filename='missing-{}.txt'.format(date.today()))
         with open('results/{}/missing.txt'.format(date.today()), 'rb') as content_file:
@@ -28,8 +27,6 @@ Have a nice day
     msg['From'] = sender
     msg['To'] = receivers
     msg['Subject'] = 'Nexus Audit report for {}!'.format(date.today())
-
-    message = ' This is a test e-mail message. '
 
     try:
         smtpObj = smtplib.SMTP('localhost')
